52a533a5af13ddc7553351c5472e63775f2701fc
nbaltofs <90826197+nbaltofs@users.noreply.github.com>
2023-12-06 20:40:36 +0100

    Display all messages and allow user to create some (#3)

--- guestbook/core/settings.py
+++ guestbook/core/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party apps
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     # Modules
     'guestbook',
 ]
@@ -124,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy forms
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
